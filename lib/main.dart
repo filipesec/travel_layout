657880ef20cf06b68b0b11df96a36fdae78e9e5c
filipesec <filipy635@gmail.com>
2026-04-19 21:40:35 -0300

    feat-APP: adiciona header e campo de busca

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,85 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(child: Text('Travel')),
+      backgroundColor: Color(0xFFEDF5FD),
+      body: SafeArea(
+        child: Column(
+          children: [
+            //header
+            Padding(padding: EdgeInsets.all(8), child: HeaderCustom()),
+
+            //campo de busca
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                height: 55,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text(
+                        'Search destination...',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+
+                    Spacer(),
+                    Icon(Icons.tune),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//custom para header
+class HeaderCustom extends StatelessWidget {
+  const HeaderCustom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundImage: AssetImage('assets/images/profile.png'),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Hello, Smith', style: TextStyle(fontSize: 15)),
+              Text(
+                'Where to next?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+            ],
+          ),
+        ),
+
+        Spacer(),
+
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+          ),
+          child: Icon(Icons.notifications_none, color: Colors.black, size: 30),
+        ),
+      ],
     );
   }
 }
