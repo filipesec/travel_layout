23fbff97154af1b4636a19e8671d93df96aa7bb2
filipesec <filipy635@gmail.com>
2026-04-21 23:28:43 -0300

    feat-APP: adiciona filtro e destinos

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,10 @@ class HomePage extends StatelessWidget {
 
             //jornada
             Padding(padding: EdgeInsets.all(8), child: JourneyCustom()),
+            //filtro
+            Padding(padding: EdgeInsets.all(8), child: FilterCustom()),
+            //destinations
+            Padding(padding: EdgeInsets.all(8), child: DestinationsCustom()),
           ],
         ),
       ),
@@ -175,3 +179,205 @@ class JourneyCustom extends StatelessWidget {
     );
   }
 }
+
+//filtro
+class FilterCustom extends StatefulWidget {
+  const FilterCustom({super.key});
+  @override
+  State<FilterCustom> createState() => _FilterCustomState();
+}
+
+class _FilterCustomState extends State<FilterCustom> {
+  final List<String> _options = ['Popular', 'Local', 'Regional', 'Global'];
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(_options.length, (index) {
+          final isSelected = _selectedIndex == index;
+          return GestureDetector(
+            onTap: () => setState(() => _selectedIndex = index),
+            child: Container(
+              width: 105,
+              height: 52,
+              margin: EdgeInsets.only(right: 12),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: isSelected ? const Color(0xFF1A1A2E) : Color(0xFFEDF5FD),
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(
+                  color: isSelected
+                      ? const Color(0xFF5A6273)
+                      : const Color(0xFFE4EBF2),
+                  width: 1.5,
+                ),
+              ),
+              child: Text(
+                _options[index],
+                style: TextStyle(
+                  color: isSelected ? Colors.white : const Color(0xFF687086),
+                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+//gestinations custom
+class DestinationsCustom extends StatelessWidget {
+  const DestinationsCustom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              'Top Destinations',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+            Spacer(),
+            Text(
+              'See all',
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                color: Color(0xFF687086),
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+
+        Row(
+          children: [
+            Expanded(
+              child: DestinationCard(
+                name: 'Santorini',
+                country: 'GR',
+                imageCountry: 'assets/images/santorini.png',
+                rating: 4.0,
+                price: 840,
+              ),
+            ),
+            Expanded(
+              child: DestinationCard(
+                name: 'Kyoto',
+                country: 'JP',
+                imageCountry: 'assets/images/kyoto.png',
+                rating: 5.0,
+                price: 500,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+//cards de destinations
+class DestinationCard extends StatelessWidget {
+  final String name;
+  final String country;
+  final String imageCountry;
+  final double rating;
+  final int price;
+
+  const DestinationCard({
+    super.key,
+    required this.name,
+    required this.country,
+    required this.imageCountry,
+    required this.rating,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(22)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(12),
+            child: Container(
+              height: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: DecorationImage(
+                  image: AssetImage(imageCountry),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              alignment: Alignment.topRight,
+              padding: EdgeInsets.all(8),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(country, style: TextStyle(fontSize: 12)),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(left: 12, right: 4, bottom: 3),
+            child: Text(
+              name,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ),
+
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 12, bottom: 12, right: 16),
+                child: Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.yellow, size: 25),
+                    Text(
+                      rating.toString(),
+                      style: TextStyle(fontSize: 17, color: Color(0xFF8A939E)),
+                    ),
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(bottom: 12),
+                child: Row(
+                  children: [
+                    Text(
+                      '\$$price',
+                      style: TextStyle(color: Color(0xFFF96D1F), fontSize: 17),
+                    ),
+
+                    Text(
+                      '/person',
+                      style: TextStyle(fontSize: 17, color: Color(0xFF8A939E)),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//consertar o problema de alinhamento do top destinations depois
